bb27ff28bd5a367f77b9a15c15cc93fdab1b79f9
rochinhaDev <you@example.com>
2023-06-27 21:04:32 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import OrderTrackingForm from "./pages/OrderTrackingForm";
+function App() {
+  return;
+  <>
+    <Routes>
+      <Route path="/" element={<OrderTrackingForm />} />
+    </Routes>
+  </>;
+}
+
+export default App;
