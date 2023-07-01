931397098e69bffe893842b4c432032eeda73d35
rochinhaDev <you@example.com>
2023-07-01 16:01:06 -0300

    tailwind

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -54,7 +54,7 @@ export default function HomePage() {
   }
 
   return (
-    <div>
+    <div className="bg-gray-100 bg-no-repeat">
       <div>
         {sales.map((sale) => (
           <div className="card flex flex-row justify-between" key={sale._id}>
@@ -66,7 +66,23 @@ export default function HomePage() {
             </p>
             <p className="border-2">Vendedor: {sale.vendedor}</p>
             <Link to={`/sales/${sale._id}`}>
-              <button className="border-2">Detalhes da venda</button>
+              <button className="text-lg border-2 hover:underline ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+                Detalhes da venda
+              </button>
             </Link>
           </div>
         ))}
